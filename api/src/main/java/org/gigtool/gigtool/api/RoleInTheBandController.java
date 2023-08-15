256090d84826216e88330c25de4c1ab7462e8deb
dario <dario.dassler@fh-erfurt.de>
2023-08-15 17:15:49 +0200

    add getAll / getByID / update

--- api/src/main/java/org/gigtool/gigtool/api/RoleInTheBandController.java
+++ api/src/main/java/org/gigtool/gigtool/api/RoleInTheBandController.java
@@ -2,12 +2,13 @@ package org.gigtool.gigtool.api;
 
 import org.gigtool.gigtool.storage.services.RoleInTheBandService;
 import org.gigtool.gigtool.storage.services.model.RoleInTheBandCreate;
+import org.gigtool.gigtool.storage.services.model.RoleInTheBandRequest;
 import org.gigtool.gigtool.storage.services.model.RoleInTheBandResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/gig/band/role")
@@ -22,5 +23,20 @@ public class RoleInTheBandController {
     @PostMapping
     public ResponseEntity<RoleInTheBandResponse> addRoleInTheBand(@RequestBody RoleInTheBandCreate roleInTheBandCreate) {
         return roleInTheBandService.addRoleInTheBand(roleInTheBandCreate);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RoleInTheBandResponse>> allRoleInTheBand(){
+        return this.roleInTheBandService.getAllRolesInTheBand();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RoleInTheBandResponse> getRoleInTheBandByID(@PathVariable UUID id){
+        return this.roleInTheBandService.getRoleInTheBandById(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<RoleInTheBandResponse> updateRoleInTheBand(@PathVariable UUID id, @RequestBody RoleInTheBandRequest roleRequest) {
+        return this.roleInTheBandService.updateRoleInTheBand(id, roleRequest);
     }
 }
--- storage/src/main/java/org/gigtool/gigtool/storage/services/RoleInTheBandService.java
+++ storage/src/main/java/org/gigtool/gigtool/storage/services/RoleInTheBandService.java
@@ -3,9 +3,14 @@ package org.gigtool.gigtool.storage.services;
 import org.gigtool.gigtool.storage.model.RoleInTheBand;
 import org.gigtool.gigtool.storage.repositories.RoleInTheBandRepository;
 import org.gigtool.gigtool.storage.services.model.RoleInTheBandCreate;
+import org.gigtool.gigtool.storage.services.model.RoleInTheBandRequest;
 import org.gigtool.gigtool.storage.services.model.RoleInTheBandResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class RoleInTheBandService {
@@ -32,6 +37,51 @@ public class RoleInTheBandService {
         return ResponseEntity.ok(new RoleInTheBandResponse(savedRoleInTheBand));
     }
 
+    public ResponseEntity<List<RoleInTheBandResponse>> getAllRolesInTheBand() {
+
+        List<RoleInTheBand> roleList = roleInTheBandRepository.findAll();
+
+        List<RoleInTheBandResponse> responseList = roleList.stream()
+                .map(RoleInTheBandResponse::new)
+                .toList();
+
+        return ResponseEntity.ok().body(responseList);
+    }
+
+    public ResponseEntity<RoleInTheBandResponse> getRoleInTheBandById(UUID roleId) {
+
+        Optional<RoleInTheBand> roleOptional = roleInTheBandRepository.findById(roleId);
+
+        return roleOptional.map(roleInTheBand -> ResponseEntity.ok(new RoleInTheBandResponse(roleInTheBand)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    public ResponseEntity<RoleInTheBandResponse> updateRoleInTheBand(UUID roleId, RoleInTheBandRequest roleRequest) {
+
+        if (roleId == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Optional<RoleInTheBand> existingRole = roleInTheBandRepository.findById(roleId);
+
+        if (existingRole.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        RoleInTheBand updatedRole = existingRole.get();
+
+        if (roleRequest.getName() != null) {
+            updatedRole.setName(roleRequest.getName());
+        }
+
+        if (roleRequest.getDescription() != null) {
+            updatedRole.setDescription(roleRequest.getDescription());
+        }
+
+        RoleInTheBand savedRole = roleInTheBandRepository.saveAndFlush(updatedRole);
+
+        return ResponseEntity.ok(new RoleInTheBandResponse(savedRole));
+    }
 
 
 }
