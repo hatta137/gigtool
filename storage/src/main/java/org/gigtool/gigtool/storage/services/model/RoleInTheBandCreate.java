f098b5e14b792f4e0928da2857352c82ee8de240
dario <dario.dassler@fh-erfurt.de>
2023-08-15 14:48:08 +0200

    create service models for RoleInTheBand

---
+++ storage/src/main/java/org/gigtool/gigtool/storage/services/model/RoleInTheBandCreate.java
@@ -0,0 +1,17 @@
+package org.gigtool.gigtool.storage.services.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class RoleInTheBandCreate {
+    private String name;
+    private String description;
+}
---
+++ storage/src/main/java/org/gigtool/gigtool/storage/services/model/RoleInTheBandRequest.java
@@ -0,0 +1,14 @@
+package org.gigtool.gigtool.storage.services.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class RoleInTheBandRequest {
+
+}
