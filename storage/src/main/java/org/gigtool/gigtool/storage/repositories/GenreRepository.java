7768274eee455e62de70c5e8593699c4500f5720
dario <dario.dassler@fh-erfurt.de>
2023-08-21 22:40:56 +0200

    implement check of count of bands with genre to check before delete

--- storage/src/main/java/org/gigtool/gigtool/storage/repositories/GenreRepository.java
+++ storage/src/main/java/org/gigtool/gigtool/storage/repositories/GenreRepository.java
@@ -2,8 +2,13 @@ package org.gigtool.gigtool.storage.repositories;
 
 import org.gigtool.gigtool.storage.model.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface GenreRepository extends JpaRepository<Genre, UUID> {
+    @Query("SELECT COUNT(b) FROM Band b WHERE b.genre.id = :genreId")
+    int countBandsWithGenre(@Param("genreId") UUID genreId);
+
 }
--- storage/src/main/java/org/gigtool/gigtool/storage/services/GenreService.java
+++ storage/src/main/java/org/gigtool/gigtool/storage/services/GenreService.java
@@ -4,6 +4,7 @@ import org.gigtool.gigtool.storage.model.Genre;
 import org.gigtool.gigtool.storage.repositories.GenreRepository;
 import org.gigtool.gigtool.storage.services.model.GenreCreate;
 import org.gigtool.gigtool.storage.services.model.GenreResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -76,15 +77,21 @@ public class GenreService {
         return ResponseEntity.ok(new GenreResponse(savedGenre));
     }
 
-    public ResponseEntity<String> deleteGenre(UUID id) {
-        if (id == null) {
+    public ResponseEntity<String> deleteGenre(UUID genreId) {
+        if (genreId == null) {
             return ResponseEntity.badRequest().body("No ID");
         }
 
-        Optional<Genre> existingGenre = genreRepository.findById(id);
+        Optional<Genre> existingGenre = genreRepository.findById(genreId);
 
         if (existingGenre.isEmpty()) {
             return ResponseEntity.notFound().build();
+        }
+
+        int bandsWithGenre = genreRepository.countBandsWithGenre(genreId);
+
+        if(bandsWithGenre > 0){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Genre has a relation to a Band. You cannot delete this Genre!");
         }
 
         genreRepository.delete(existingGenre.get());
