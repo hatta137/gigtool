282fdad523f8c1a97416c70ea04ae342b014667a
dario <dario.dassler@fh-erfurt.de>
2023-08-22 22:18:54 +0200

    noBody needs timsLots

--- storage/src/main/java/org/gigtool/gigtool/storage/repositories/TimeslotRepository.java
+++
@@ -1,9 +0,0 @@
-package org.gigtool.gigtool.storage.repositories;
-
-import org.gigtool.gigtool.storage.model.Timeslot;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface TimeslotRepository extends JpaRepository<Timeslot, UUID> {
-}
