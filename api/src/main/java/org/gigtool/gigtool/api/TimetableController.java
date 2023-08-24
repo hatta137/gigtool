04bdc731ff9f4dd9180860fdfdff90a94451b759
dario <dario.dassler@fh-erfurt.de>
2023-08-24 03:33:49 +0200

    FINISH TIMETABLE

--- api/src/main/java/org/gigtool/gigtool/api/TimetableController.java
+++ api/src/main/java/org/gigtool/gigtool/api/TimetableController.java
@@ -1,13 +1,16 @@
 package org.gigtool.gigtool.api;
 
+import org.gigtool.gigtool.storage.model.Happening;
 import org.gigtool.gigtool.storage.services.TimetableService;
+import org.gigtool.gigtool.storage.services.model.CalcResponse;
+import org.gigtool.gigtool.storage.services.model.LocationResponse;
 import org.gigtool.gigtool.storage.services.model.TimetableResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/timetable")
@@ -22,6 +25,23 @@ public class TimetableController {
     @GetMapping
     public ResponseEntity<List<TimetableResponse>> getAllHappenings() {
         return this.timetableService.getAll();
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<TimetableResponse>> getFilteredHappenings(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String description) {
+        return this.timetableService.getFilteredHappenings(name, description);
+    }
+
+    @GetMapping("/{id}/calc")
+    public ResponseEntity<CalcResponse> getTotalValuesOfHappening(@PathVariable UUID id) {
+        return this.timetableService.getTotalValuesOfHappening(id);
+    }
+
+    @GetMapping("/{id}/whereismyequipment")
+    public ResponseEntity<List<LocationResponse>> whereIsMyEquipment(@PathVariable UUID id) {
+        return this.timetableService.getLocationsOfEquiptmentFromHappening(id);
     }
 
 }
--- storage/src/main/java/org/gigtool/gigtool/storage/repositories/HappeningRepository.java
+++ storage/src/main/java/org/gigtool/gigtool/storage/repositories/HappeningRepository.java
@@ -1,9 +1,11 @@
 package org.gigtool.gigtool.storage.repositories;
 
+import org.gigtool.gigtool.storage.model.Address;
 import org.gigtool.gigtool.storage.model.Equipment;
 import org.gigtool.gigtool.storage.model.Happening;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -18,7 +20,12 @@ public interface HappeningRepository extends JpaRepository<Happening, UUID> {
             WHERE (h.startTime <= :endTime AND h.endTime >= :startTime)
             AND (:equipment MEMBER OF h.equipmentList
             OR (:equipment MEMBER OF b.equipmentList))""")
-    List<Happening> findOverlappingHappeningsWithEquipment(LocalDateTime startTime,
-                                                           LocalDateTime endTime,
-                                                           Equipment equipment);
+    List<Happening> findOverlappingHappeningsWithEquipment(@Param("startTime") LocalDateTime startTime, @Param("endTime") LocalDateTime endTime,
+                                                           @Param("equipment") Equipment equipment);
+
+    @Query(""" 
+            SELECT h FROM Happening h WHERE (:name IS NULL OR LOWER(h.name) LIKE LOWER(concat('%', :name, '%'))
+            AND :description IS NULL OR LOWER(h.description) LIKE LOWER(concat('%', :description, '%')))
+            """)
+    List<Happening> findFilteredHappenings(String name, String description);
 }
